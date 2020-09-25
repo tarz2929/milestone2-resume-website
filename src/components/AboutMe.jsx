694625c40bdbd697599114f01a982c96199730f6
tarz2929 <saitarunmulley@gmail.com>
2020-09-25 15:49:30 -0600

    added bio to AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,28 @@ import React from 'react';
 function AboutMe(){
     return(
         <>
-        <h1>Greetings from Tarun</h1>
+         <div>
+        <div className="content">
+
+            <h2> Who am I? </h2>
+          
+          <p>Born in Hyderabad, India.</p>
+          <p>Enthusiastic professional with a solution seeking mindset towards any problem. Aspiring to start a new career
+            with the tech sector and hoping to solve societal problems using technology. Some of my interests include
+            public speaking, human psychology and yoga. And, I am Constantly looking for ways to make my work as an
+            expression of
+            joy. </p>
+          <p>Having worked in the field of Retail, Engineering and Project management I am able to bring the best of my
+            skills from diverse backgrounds.
+            When I am not working, you will see me either spending close time with nature or experimenting amateur
+            photography on my iPhone.
+          </p>
+          
+          
+          
+        </div>
+        
+      </div>
         </>
     );
 }
