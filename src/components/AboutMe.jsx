71590ba56b27debc8dcb7077cc5b0d8a5b27003c
tarz2929 <saitarunmulley@gmail.com>
2020-09-25 12:53:03 -0600

    added about me feature/Code is working

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AboutMe(){
+    return(
+        <>
+        <h1>Greetings from Tarun</h1>
+        </>
+    );
+}
+export default AboutMe;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import AboutMe from './components/AboutMe'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <AboutMe/>
+
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
