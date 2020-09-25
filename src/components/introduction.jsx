c68b356ff56e1136396ff816567ffddfba76fbf2
tarz2929 <saitarunmulley@gmail.com>
2020-09-25 13:10:41 -0600

    added HTML and basic CSS prototype for all pages

---
+++ src/components/introduction.jsx
@@ -0,0 +1,24 @@
+//Import React
+import React from 'react';
+
+//Declare Function
+function Introduction() {
+    return(
+        <>
+            <main>
+                <section className="main-section">
+                    <h2>
+                        
+                        Introduction
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est velit egestas dui id. Tristique senectus et netus et malesuada fames ac. Tortor condimentum lacinia quis vel. Non curabitur gravida arcu ac tortor dignissim. Donec et odio pellentesque diam volutpat. Vitae congue eu consequat ac. Purus faucibus ornare suspendisse sed nisi lacus. Platea dictumst quisque sagittis purus sit amet volutpat consequat mauris. Tellus at urna condimentum mattis pellentesque id nibh. Orci nulla pellentesque dignissim enim. Vitae justo eget magna fermentum. Proin sagittis nisl rhoncus mattis.
+                    </p>
+                    
+                </section>
+            </main>
+        </>
+    );
+}
+//Export Function
+export default Introduction;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AboutMe from './components/AboutMe'
+import Introduction from './components/Introduction'
+import Timeline from './components/Timeline'
+import Resume from './components/Resume'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <AboutMe/>
+    <Introduction/>
+    <Timeline/>
+    <Resume/>
 
   
   </React.StrictMode>,
