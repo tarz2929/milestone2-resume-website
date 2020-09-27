9b65f5b183b89ce45ed2ed23cedc71c668d0bae3
tarz2929 <saitarunmulley@gmail.com>
2020-09-26 23:47:07 -0600

    installed and imported Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import AboutMe from './components/AboutMe'
 import Introduction from './components/Introduction'
 import Timeline from './components/Timeline'
 import Resume from './components/Resume'
 import Sidebar from './components/Sidebar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 
 ReactDOM.render(
