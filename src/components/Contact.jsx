b7fed70a62259acb9b6cd57d7bb88abe6ecefbd9
tarz2929 <saitarunmulley@gmail.com>
2020-09-28 13:45:19 -0600

    declared const onSubmit and changed input type as submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,12 +7,15 @@ function ContactMe() {
     const [email, setEmail] = useState("")
     const [subject, setSubject] = useState("")
     const [comments, setComments] =useState("")
+    const onSubmit = () =>{
+        
+    }
 
 
     return (
 
         <section id="ContactMe"><div className="container">
-            <form action="mailto:mail@tarun.com" method="get" encType="text/plain">
+            <form onSubmit={onSubmit} action="mailto:mail@tarun.com" method="get" encType="text/plain">
                 <div>
                     <label htmlFor="name">Name:
             <input type="text" name="name" id="name" />
@@ -34,7 +37,7 @@ function ContactMe() {
                     <textarea name="comments" id="message" rows={12} cols={35} placeholder="Please send your message here." defaultValue={""} />
                 </div>
                 <div>
-                    <input type="button" id="send-email" onclick="validateMeessage()" name="submit" defaultValue="Send" />
+                    <input type="submit" id="send-email" onclick="validateMeessage()" name="submit" defaultValue="Send" />
                     <input type="reset" name="reset" defaultValue="Clear Form" />
                 </div>
             </form>
